6858f83668a7b3a4ec5faa6a5bf341b182e2c57b
Chris Mittell <mittell@hotmail.co.uk>
2022-09-03 13:38:54 +0900

    feat: added Layout component and base styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Layout from '../components/Layout/Layout';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Flag App</title>
-				<meta name='description' content='A Flag App built with Next.js' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-
-			<main className={styles.main}>Main</main>
-
-			<footer className={styles.footer}>Footer</footer>
-		</div>
-	);
+	return <Layout>Main</Layout>;
 }
