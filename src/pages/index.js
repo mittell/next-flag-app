5b25bd9e59b9b942f708dbaae98c71e299f8a315
Chris Mittell <mittell@hotmail.co.uk>
2022-09-03 14:06:51 +0900

    feat: added SearchInput component and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Layout from '../components/Layout/Layout';
+import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-	return <Layout>Main</Layout>;
+export default function Home({ countries }) {
+	console.log(countries);
+	return (
+		<Layout>
+			<div className={styles.count}>Found {countries.length} countries</div>
+			<SearchInput placeholder='Filter by Name, Region, or Subregion' />
+		</Layout>
+	);
 }
+
+export const getStaticProps = async () => {
+	const res = await fetch('https://restcountries.com/v3.1/all');
+	const countries = await res.json();
+
+	return {
+		props: {
+			countries,
+		},
+	};
+};
