000032122c3127a1eeecd966c9fb3d42ae911f1a
Chris Mittell <mittell@hotmail.co.uk>
2022-09-05 14:19:57 +0900

    feat: added filtering logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,35 @@
+import { useState } from 'react';
 import CountriesTable from '../components/CountriesTable/CountriesTable';
 import Layout from '../components/Layout/Layout';
 import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ countries }) {
-	console.log(countries);
+	const [keyword, setKeyword] = useState('');
+
+	const filteredCounties = countries.filter(
+		(country) =>
+			country.name.common.toLowerCase().includes(keyword) ||
+			country.region.toLowerCase().includes(keyword) ||
+			country.subregion.toLowerCase().includes(keyword)
+	);
+
+	const onInputChange = (e) => {
+		e.preventDefault();
+
+		setKeyword(e.target.value.toLowerCase());
+	};
+
 	return (
 		<Layout>
 			<div className={styles.count}>Found {countries.length} countries</div>
-			<SearchInput placeholder='Filter by Name, Region, or Subregion' />
-			<CountriesTable countries={countries} />
+
+			<SearchInput
+				placeholder='Filter by Name, Region, or Subregion'
+				onChange={onInputChange}
+			/>
+
+			<CountriesTable countries={filteredCounties} />
 		</Layout>
 	);
 }
