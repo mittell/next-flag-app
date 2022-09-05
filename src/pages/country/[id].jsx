97be38282ef8f5800af5986368f972f55ca53001
Chris Mittell <mittell@hotmail.co.uk>
2022-09-05 16:32:18 +0900

    feat: updated layout and flag scaling

--- src/pages/country/[id].jsx
+++ src/pages/country/[id].jsx
@@ -20,7 +20,6 @@ const Country = ({ country }) => {
 				country.borders.map((border) => getCountry(border))
 			);
 
-			console.log(border);
 			setBorders(border);
 		}
 	};
@@ -32,105 +31,113 @@ const Country = ({ country }) => {
 
 	return (
 		<Layout title={country.name.common}>
-			<div>
-				<div className={styles.overview_panel}>
-					<img src={country.flags.svg} alt={country.name.common} />
-					<h1 className={styles.overview_name}>{country.name.common}</h1>
-					<div className={styles.overview_region}>{country.region}</div>
+			<div className={styles.container}>
+				<div className={styles.container_left}>
+					<div className={styles.overview_panel}>
+						<img src={country.flags.png} alt={country.name.common} />
+						<h1 className={styles.overview_name}>{country.name.common}</h1>
+						<div className={styles.overview_region}>{country.region}</div>
 
-					<div className={styles.overview_numbers}>
-						<div className={styles.overview_population}>
-							<div className={styles.overview_value}>{country.population}</div>
-							<div className={styles.overview_label}>Population</div>
-						</div>
+						<div className={styles.overview_numbers}>
+							<div className={styles.overview_population}>
+								<div className={styles.overview_value}>
+									{country.population}
+								</div>
+								<div className={styles.overview_label}>Population</div>
+							</div>
 
-						<div className={styles.overview_area}>
-							<div className={styles.overview_value}>{country.area}</div>
-							<div className={styles.overview_label}>Area</div>
+							<div className={styles.overview_area}>
+								<div className={styles.overview_value}>{country.area}</div>
+								<div className={styles.overview_label}>Area</div>
+							</div>
 						</div>
 					</div>
 				</div>
 
-				<div className={styles.details_panel}>
-					<h4 className={styles.details_panel_heading}>Details</h4>
+				<div className={styles.container_right}>
+					<div className={styles.details_panel}>
+						<h4 className={styles.details_panel_heading}>Details</h4>
 
-					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Capital</div>
-						<div className={styles.details_panel_value}>{country.capital}</div>
-					</div>
-
-					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Subregion</div>
-						<div className={styles.details_panel_value}>
-							{country.subregion}
-						</div>
-					</div>
-
-					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Languages</div>
-						<div className={styles.details_panel_value}>
-							{Object.values(country.languages).join(', ')}
-						</div>
-					</div>
-
-					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Currencies</div>
-						<div className={styles.details_panel_value}>
-							{Object.values(country.currencies)
-								.flatMap((cur) => cur['name'])
-								.toString()
-								.replace(',', ', ')}
-						</div>
-					</div>
-
-					<div className={styles.details_panel_row}>
-						<div className={styles.details_panel_label}>Native names</div>
-						<div className={styles.details_panel_value}>
-							{Object.values(country.name.nativeName)
-								.flatMap((cur) => cur['official'])
-								.toString()
-								.replace(',', ', ')}
-						</div>
-					</div>
-
-					{country.gini && (
 						<div className={styles.details_panel_row}>
-							<div className={styles.details_panel_label}>Gini</div>
+							<div className={styles.details_panel_label}>Capital</div>
 							<div className={styles.details_panel_value}>
-								{country.gini &&
-									Object.values(country.gini)
-										.flatMap((cur) => {
-											return cur.toString() + '%';
-										})
-										.toString()
-										.replace(',', ', ')}
+								{country.capital}
 							</div>
 						</div>
-					)}
 
-					{borders.length > 0 && (
-						<div className={styles.details_panel_borders}>
-							<div className={styles.details_panel_borders_label}>
-								Neighbouring Countries
-							</div>
-							<div className={styles.details_panel_borders_container}>
-								{borders.map((border, index) => (
-									<div
-										className={styles.details_panel_borders_country}
-										key={index}
-									>
-										<img
-											src={border[0].flags.svg}
-											alt={border[0].name.common}
-										/>
-										<div className={styles.details_panel_borders_name}>
-											{border[0].name.common}
-										</div>
-									</div>
-								))}
+						<div className={styles.details_panel_row}>
+							<div className={styles.details_panel_label}>Subregion</div>
+							<div className={styles.details_panel_value}>
+								{country.subregion}
 							</div>
 						</div>
-					)}
+
+						<div className={styles.details_panel_row}>
+							<div className={styles.details_panel_label}>Languages</div>
+							<div className={styles.details_panel_value}>
+								{Object.values(country.languages).join(', ')}
+							</div>
+						</div>
+
+						<div className={styles.details_panel_row}>
+							<div className={styles.details_panel_label}>Currencies</div>
+							<div className={styles.details_panel_value}>
+								{Object.values(country.currencies)
+									.flatMap((cur) => cur['name'])
+									.toString()
+									.replace(',', ', ')}
+							</div>
+						</div>
+
+						<div className={styles.details_panel_row}>
+							<div className={styles.details_panel_label}>Native names</div>
+							<div className={styles.details_panel_value}>
+								{Object.values(country.name.nativeName)
+									.flatMap((cur) => cur['official'])
+									.toString()
+									.replace(',', ', ')}
+							</div>
+						</div>
+
+						{country.gini && (
+							<div className={styles.details_panel_row}>
+								<div className={styles.details_panel_label}>Gini</div>
+								<div className={styles.details_panel_value}>
+									{country.gini &&
+										Object.values(country.gini)
+											.flatMap((cur) => {
+												return cur.toString() + '%';
+											})
+											.toString()
+											.replace(',', ', ')}
+								</div>
+							</div>
+						)}
+
+						{borders.length > 0 && (
+							<div className={styles.details_panel_borders}>
+								<div className={styles.details_panel_borders_label}>
+									Neighbouring Countries
+								</div>
+								<div className={styles.details_panel_borders_container}>
+									{borders.map((border, index) => (
+										<div
+											className={styles.details_panel_borders_country}
+											key={index}
+										>
+											<img
+												src={border[0].flags.png}
+												alt={border[0].name.common}
+											/>
+											<div className={styles.details_panel_borders_name}>
+												{border[0].name.common}
+											</div>
+										</div>
+									))}
+								</div>
+							</div>
+						)}
+					</div>
 				</div>
 			</div>
 		</Layout>
